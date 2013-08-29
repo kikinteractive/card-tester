ba2f92c1dbdac4cc64eba7ae8023c84f0e7dfb9f
Ben Chilibeck <chilibeck@gmail.com>
2013-08-29 15:48:45 -0400

    Initial dump --- very WIP

---
+++ web/card_verification.js
@@ -0,0 +1,176 @@
+var fs     = require('fs'),
+	page   = require('webpage').create(),
+	system = require('system');
+
+var url = "http://cards-image-search-dev.herokuapp.com",
+	t   = Date.now(),
+	tt  = Date.now(),
+	logFilter = "#######CARDTESTER#######",
+	id;
+
+//url = "http://static.tresensa.com/run-and-bun/index.html?dst=A0020";
+//url = "http://youtube.kik.com";
+
+if (system.args.length === 1) {
+    console.log('Usage: card_verification.js <some URL> <base64 url>');
+    phantom.exit(1);
+} else {
+	url = system.args[1];
+	id = system.args[2];
+}
+
+page.settings.userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3";
+page.viewportSize = { width: 320, height : 548 };
+page.resources = [];
+
+page.onConsoleMessage = function (msg) {
+	if ( msg.indexOf(logFilter) === 0 ) {
+    	console.log(msg.replace(logFilter, ""));
+	}
+};
+
+function printArgs() {
+    var i, ilen;
+    for (i = 0, ilen = arguments.length; i < ilen; ++i) {
+        //console.log("    arguments[" + i + "] = " + JSON.stringify(arguments[i]));
+    }
+    //console.log("");
+}
+
+page.onInitialized = function() {
+    //console.log("page.onInitialized");
+    printArgs.apply(this, arguments);
+
+	page.evaluate(function(domContentLoadedMsg) {
+		document.addEventListener('DOMContentLoaded', function() {
+			window.callPhantom('DOMContentLoaded');
+		}, false);
+	});
+};
+
+page.onCallback = function(data) {
+	//console.log('DOMContentLoaded');
+	tt = Date.now() - tt;
+	//console.log('Loading time ' + tt + ' msec');
+};
+
+page.onLoadStarted = function() {
+    //console.log("page.onLoadStarted");
+    printArgs.apply(this, arguments);
+    page.startTime = new Date();
+};
+
+page.onLoadFinished = function() {
+    //console.log("page.onLoadFinished");
+    printArgs.apply(this, arguments);
+};
+
+page.onResourceRequested = function (req) {
+	page.resources[req.id] = {
+		request: req,
+		startReply: null,
+		endReply: null
+	};
+};
+
+page.onResourceReceived = function (res) {
+	if (res.stage === 'start') {
+		page.resources[res.id].startReply = res;
+	}
+	if (res.stage === 'end') {
+		page.resources[res.id].endReply = res;
+	}
+};
+
+page.open(url, function (status) {
+
+	var loadTime;
+
+	if (status !== 'success') {
+        //console.log('FAIL to load the address');
+    } else {
+        loadTime = Date.now() - t;
+        //console.log('Page.open Loading time ' + loadTime + ' msec');
+    }
+
+    page.render('screens/' + id + '.png');
+
+	var cardReport = {
+		more: {
+			includeInMore: false
+		},
+		load: {
+			time: loadTime
+		},
+		link: {
+		}
+	};
+
+	var resources = [];
+	var size = 0;
+
+	page.resources.forEach(function (resource) {
+		if ( !resource.request.url.match(/(^data:image\/.*)/i) ) {
+			resources.push(resource.request.url);
+			
+			if ( resource.startReply ) {
+				//console.log(JSON.stringify(resource.startReply));
+				size += resource.startReply.bodySize;
+			}
+		}
+	});
+
+	cardReport.load.resources = resources;
+	cardReport.load.requestCount = resources.length;
+	cardReport.load.cardSize = size;
+
+	cardReport.more.title = page.evaluate(function(cardReport) {
+		return document.title;
+	});
+
+	page.evaluate(function (cardReport, logFilter) {
+
+		var metaTags = document.head.childNodes;
+
+		for (var i = 0; i < metaTags.length; i++) {
+			var tag = metaTags[i];
+
+			if ( tag.tagName == "META" || tag.tagName == "LINK" ) {
+
+				if ((tag.rel === "kik-icon") && (tag.href || '').trim()) {
+					cardReport.more.icon = tag.href;
+				}
+
+				if ((tag.name === "description") && (tag.content || '').trim()) {
+					cardReport.more.description = tag.content;
+				}
+
+				if ((tag.name === "kik-more") && (tag.content || '').trim()) {
+					cardReport.more.includeInMore = true;
+					cardReport.more.hostname = tag.content;
+				}
+
+				if ((tag.name === "kik-unsupported") && (tag.content || '').trim()) {
+					cardReport.unsupported = tag.content;
+				}
+
+				if ((tag.rel === "privacy") && (tag.href || '').trim()) {
+					cardReport.link.privacy = tag.href;
+				}
+
+				if ((tag.rel === "terms") && (tag.href || '').trim()) {
+					cardReport.link.terms = tag.href;
+				}
+			}
+		}
+
+		//console.log("-----------------");
+		console.log(logFilter + JSON.stringify(cardReport));
+		//console.log("-----------------");
+	}, cardReport, logFilter);
+
+	setTimeout(function(){
+		page.render('screens/' + id + '2.png');
+		phantom.exit();
+	}, 1250);
+});
---
+++ web/zerver/API.js
@@ -0,0 +1,204 @@
+
+var phantom = require('node-phantom');
+
+exports.testCard = testCard2;
+exports._crossOrigin = '*';
+
+
+function testCard(url, callback) {
+
+	phantom.create(function(err,ph) {
+
+		//console.log("ph: " + JSON.stringify(ph));
+
+		for (var a in ph._phantom ) {
+			console.log(a);
+		}
+
+		//ph.settings.userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3";
+		//ph.viewportSize = { width: 320, height : 548 };
+
+		return ph.createPage(function(err,page) {
+
+			//console.log("page: " + JSON.stringify(page));
+			//console.log("page: " + JSON.stringify(err));
+			
+			page.resources = [];
+
+			return page.open(url, function(err,status) {
+
+				//console.log("opened site? ", status);
+				//console.log("page: " + JSON.stringify(page));
+
+				page.render('temp/example.png');
+
+				var cardReport = {
+					more: {
+						includeInMore: false
+					},
+					load: {
+						//time: loadTime
+					}
+				};
+
+				// var resources = [];
+				// var size = 0;
+
+				// page.resources.forEach(function (resource) {
+				// 	if ( !resource.request.url.match(/(^data:image\/.*)/i) ) {
+				// 		resources.push(resource.request.url);
+						
+				// 		if ( resource.startReply ) {
+				// 			console.log(JSON.stringify(resource.startReply));
+				// 			size += resource.startReply.bodySize;
+				// 		}
+				// 	}
+				// });				
+
+				// cardReport.load.resources = resources;
+				// cardReport.load.requestCount = resources.length;
+				// cardReport.load.cardSize = size;
+
+				// cardReport.more.title = page.evaluate(function(cardReport) {
+				// 	return document.title;
+				// });
+
+				page.evaluate(function(s){
+					return document.querySelector(s).innerText;
+				},function(err,title){
+					cardReport.more.title = title;
+				},'title');
+
+				page.evaluate(function (cardReport) {
+
+					var metaTags = document.head.childNodes;
+
+					for (var i = 0; i < metaTags.length; i++) {
+						var tag = metaTags[i];
+
+						if ( tag.tagName == "META" || tag.tagName == "LINK" ) {
+
+							if ((tag.rel === "kik-icon") && (tag.href || '').trim()) {
+								cardReport.more.icon = tag.href;
+							}
+
+							if ((tag.name === "description") && (tag.content || '').trim()) {
+								cardReport.more.description = tag.content;
+							}
+
+							if ((tag.name === "kik-more") && (tag.content || '').trim()) {
+								cardReport.more.includeInMore = true;
+								cardReport.more.hostname = tag.content;
+							}
+
+							if ((tag.name === "kik-unsupported") && (tag.content || '').trim()) {
+								cardReport.unsupported = tag.content;
+							}
+						}
+					}
+
+					// console.log("-----------------");
+					// console.log(JSON.stringify(cardReport));
+					// console.log("-----------------");
+
+					return cardReport;
+
+				}, function(err, data){
+					cardReport = data;
+				}, cardReport);
+
+				callback(cardReport);
+
+				// page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function(err) {
+				// 	//jQuery Loaded.
+				// 	//Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds.
+
+				// 	callback({pageOpened: true});
+
+				// 	setTimeout(function() {
+				// 		return page.evaluate(function() {
+				// 			//Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object.
+				// 			var h2Arr = [],
+				// 			pArr = [];
+
+				// 			$('h2').each(function() {
+				// 				h2Arr.push($(this).html());
+				// 			});
+
+				// 			$('p').each(function() {
+				// 				pArr.push($(this).html());
+				// 			});
+
+				// 			return {
+				// 				h2: h2Arr,
+				// 				p: pArr
+				// 			};
+				// 		}, function(err,result) {
+				// 			console.log(result);
+
+				// 			ph.exit();
+				// 		});
+				// 	}, 5000);
+				// });
+
+			});
+		});
+	});
+}
+
+function testCard2(url, callback) {
+
+	var childProcess = require('child_process'),
+		ls;
+
+	var id = new Buffer(url).toString('base64');
+
+	id = id.replace(/=/g, "");
+
+	if ( id.length > 30 ) {
+		id = id.substr(0, 30);
+	}
+
+	ls = childProcess.exec('phantomjs card_verification.js ' + url + ' ' + id + ' > data/' + id + '.txt', function (error, stdout, stderr) {
+		if (error) {
+			console.log(error.stack);
+			console.log('Error code: '+error.code);
+			console.log('Signal received: '+error.signal);
+		}
+		//console.log('Child Process STDOUT: '+stdout);
+		//console.log('Child Process STDERR: '+stderr);
+	});
+
+	ls.on('exit', function (code) {
+		console.log('Child process exited with exit code '+code);
+		var fs = require('fs');
+
+		// fs.readFile('data/' + id + '.txt', 'utf8', function (err,data) {
+		//   if (err) {
+		//     return console.log(err);
+		//   }
+		//   //console.log(data);
+		//   try {
+		//   	var d = JSON.parse(data);
+
+		//   	d.screenshot = "/screens/" + id + ".png";
+
+		//   	callback(d);
+		//   } catch (err) {
+		//   	callback();
+		//   }
+		// });
+		
+		var text = fs.readFileSync('data/' + id + '.txt','utf8');
+
+		if ( text ) {
+			var d = JSON.parse(text);
+			d.screenshot = "/screens/" + id + ".png";
+			d.screenshot2 = "/screens/" + id + "2.png";
+			callback(d);
+		} else {
+			callback();
+		}
+	});
+
+}
