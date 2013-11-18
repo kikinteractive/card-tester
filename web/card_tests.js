63863e187af07017cc6530fa7ac58bc5d06763c7
Jairaj Sethi <j@jairaj.org>
2013-11-18 11:39:14 -0500

    refactor

--- web/card_tests.js
+++ web/card_tests.js
@@ -1,37 +1,43 @@
-var	system = require('system');
+var fs      = require('fs'),
+	webpage = require('webpage'),
+	system  = require('system');
 
-if (system.args.length === 1) {
-	console.log('Usage: card_tests.js <some URL>');
-	phantom.exit(1);
-} else {
-	var url = system.args[1];
-	runTests(url, function(data){
-		runTests(url, function(data){
+var IOS_5       = 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3',
+	ANDROID_4_2 = 'Mozilla/5.0 (Linux; U; Android 4.2; en-us; Nexus 4 Build/JOP24G) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+	SWEAR_WORDS = /\sasshole\s|\sbitch\s|\scunt\s|\sdamn\s|\sdick\s|\sdyke\s|\sfaggot\s|\sfuck\s|\sfuckass\s|\snigger\s|\spenis\s|\spussy\s|\sshit\s|\ssex\s|\sspic\s|\sshit\s|\svagina\s|\swhore\s/gi;
+
+main.apply(system, system.args);
+
+
+
+function main(url) {
+	if ( !url ) {
+		console.log('Usage: card_tests.js <some URL>');
+		phantom.exit(1);
+		return;
+	}
+
+	runTests(url, function (data) {
+		runTests(url, function (data) {
 			console.log(data);
 			phantom.exit(0);
 		});
 	});
 }
 
-
-
 function runTests(url, callback) {
-	var fs     = require('fs'),
-		page   = require('webpage').create();
-
-	var beginTime   = Date.now(),
-		logFilter   = "#######CARDTESTER#######",
+	var page        = webpage.create(),
+		beginTime   = Date.now(),
+		logFilter   = '#######CARDTESTER#######',
 		domLoaded   = false,
 		domLoadTime, fullLoadTime;
-
-	var SWEAR_WORDS = /\sasshole\s|\sbitch\s|\scunt\s|\sdamn\s|\sdick\s|\sdyke\s|\sfaggot\s|\sfuck\s|\sfuckass\s|\snigger\s|\spenis\s|\spussy\s|\sshit\s|\ssex\s|\sspic\s|\sshit\s|\svagina\s|\swhore\s/gi;
 
 	page.settings.localToRemoteUrlAccessEnabled = true;
 	page.settings.webSecurityEnabled = false;
 	page.settings.clearMemoryCaches = true;
 	page.settings.appCache = false;
-	page.settings.userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3";
-	page.settings.userAgent = "Mozilla/5.0 (Linux; U; Android 4.2; en-us; Nexus 4 Build/JOP24G) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30";
+	page.settings.userAgent = IOS_5;
+	page.settings.userAgent = ANDROID_4_2;
 
 	page.resources = [];
 	page.appCache = false;
@@ -39,17 +45,16 @@ function runTests(url, callback) {
 	page.viewportSize = { width: 320, height : 548 };
 
 	page.onConsoleMessage = function (msg) {
-		if ( msg.indexOf(logFilter) === 0 ) {
-			console.log(msg.replace(logFilter, ""));
+		if (msg.indexOf(logFilter) === 0) {
+			console.log(msg.replace(logFilter, ''));
 		}
 	};
 
-	page.onError = function(msg) {
+	page.onError = function (msg) {
 		return false;
 	};
 
-	page.onInitialized = function() {
-
+	page.onInitialized = function () {
 		page.evaluate(function(domContentLoadedMsg) {
 			document.addEventListener('DOMContentLoaded', function() {
 				window.callPhantom('DOMContentLoaded');
@@ -63,37 +68,28 @@ function runTests(url, callback) {
 		});
 	};
 
-	page.onCallback = function(data) {
-
+	page.onCallback = function (data) {
 		var newDiff = Date.now() - beginTime;
 
-		if ( data === "DOMContentLoaded" ) {
+		if (data === 'DOMContentLoaded') {
 			domLoadTime = newDiff;
 			domLoaded = true;
-		} else if ( data === "load" ) {
+		} else if (data === 'load') {
 			fullLoadTime = newDiff;
 		}
-
-		//console.log(data + ' time ' + newDiff + ' msec');
 	};
 
 	page.onLoadStarted = function() {
-		page.startTime = new Date();
-		//console.log(logFilter + "page.onLoadStarted");
-	};
-
-	page.onLoadFinished = function (page, config, status) {
-		//console.log(logFilter + "page.onLoadFinished");
+		page.startTime = new Date(); //TODO: why?
 	};
 
 	page.onResourceRequested = function (req) {
 		page.resources[req.id] = {
 			request: req,
 			startReply: null,
-			endReply: null
+			endReply: null,
+			domLoaded: domLoaded
 		};
-
-		page.resources[req.id].domLoaded = domLoaded;
 	};
 
 	page.onResourceReceived = function (res) {
@@ -106,9 +102,6 @@ function runTests(url, callback) {
 	};
 
 	page.open(url, function (status) {
-
-		var loadTime = Date.now() - beginTime;
-
 		if (status !== 'success') {
 			console.log(logFilter + "__FAILEDTOLOAD__");
 			callback('');
@@ -124,13 +117,11 @@ function runTests(url, callback) {
 				domLoad: domLoadTime,
 				fullLoad: fullLoadTime
 			},
-			link: {
-			},
-			layout: {
-			}
+			link: {},
+			layout: {}
 		};
 
-		cardReport.screenshot = generateDataURL(page.renderBase64());
+		cardReport.screenshot = generateDataURL(page);
 
 		cardReport.more.title = page.evaluate(function() {
 			return document.title;
@@ -206,51 +197,46 @@ function runTests(url, callback) {
 
 		}, cardReport, logFilter, SWEAR_WORDS);
 
-		cardReport.load.manifest = page.evaluate(function(){
-			return document.querySelectorAll('html')[0].getAttribute("manifest");
+		cardReport.load.manifest = page.evaluate(function () {
+			return document.querySelectorAll('html')[0].getAttribute('manifest');
 		});
 
-		cardReport.more.pg13 = page.evaluate(function(SWEAR_WORDS){
-			return !SWEAR_WORDS.test(document.querySelector("html").textContent);
+		cardReport.more.pg13 = page.evaluate(function (SWEAR_WORDS) {
+			return !SWEAR_WORDS.test(document.querySelector('html').textContent);
 		}, SWEAR_WORDS);
 
-		cardReport.layout.topbar_android = page.evaluate(function(){
+		cardReport.layout.topbar_android = page.evaluate(function () {
 			var topBar = document.querySelectorAll('.app-topbar');
-
 			if ( topBar.length ) {
 				return topBar[0].clientHeight;
 			}
-
 			return null;
 		});
 
-		cardReport.layout.topbar_ios = page.evaluate(function(){
-
+		cardReport.layout.topbar_ios = page.evaluate(function () {
 			var height = null,
 				topBar = document.querySelectorAll('.app-topbar'),
-				bodyClassList = document.querySelector("body").classList;
+				body   = document.querySelector('body');
 
-			bodyClassList.remove("app-android");
-			bodyClassList.add("app-ios");
+			body.classList.remove('app-android');
+			body.classList.add('app-ios');
 
 			if ( topBar.length ) {
 				height = topBar[0].clientHeight;
 			}
 
-			bodyClassList.add("app-android");
-			bodyClassList.remove("app-ios");
+			body.classList.add('app-android');
+			body.classList.remove('app-ios');
 
 			return height;
 		});
 
-		var resources = [];
-		var size = 0;
-		var fullSize = 0;
+		var resources = [],
+			size      = 0,
+			fullSize  = 0;
 
 		page.resources.forEach(function (resource) {
-
 			if ( !resource.request.url.match(/(^data:image\/.*)/i) && !resource.request.url.match(/(^http:\/\/cardsbridge.kik.com\/.*)/i) && isFirstFetch(resource.request.url) ) {
-
 				resources.push(resource);
 
 				if ( resource.startReply ) {
@@ -274,37 +260,28 @@ function runTests(url, callback) {
 			}
 		});
 
-		cardReport.load.resources = resources;
+		cardReport.load.resources    = resources;
 		cardReport.load.requestCount = resources.length;
-		cardReport.load.cardSize = size;
-		cardReport.load.fullSize = fullSize;
+		cardReport.load.cardSize     = size;
+		cardReport.load.fullSize     = fullSize;
 
-		var start = new Date().getTime();
-
-		worstHackEver(2000);
+		sleep(2);
 
 		setTimeout(function(){
-
-			cardReport.screenshot2 = generateDataURL(page.renderBase64());
-
-			worstHackEver(1200);
-
-			//console.log(logFilter + JSON.stringify(cardReport));
+			cardReport.screenshot2 = generateDataURL(page);
 
 			callback(JSON.stringify(cardReport));
-
-			//phantom.exit();
-
 		}, 4250);
-
-		function worstHackEver(time) {
-			while ( (new Date().getTime()) - start < time ) {
-				start = start;
-			}
-		}
-
-		function generateDataURL(data) {
-			return "data:image/png;base64," + data;
-		}
 	});
+}
+
+function sleep(seconds) {
+	var start = Date.now();
+	while (Date.now()-start < seconds*1000) {
+		start = start;
+	}
+}
+
+function generateDataURL(page) {
+	return "data:image/png;base64," + page.renderBase64();
 }
