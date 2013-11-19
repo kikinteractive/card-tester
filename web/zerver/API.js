cc0a2b3f31b75d9aa0e6b510fa90c777029d1dc0
Jairaj Sethi <j@jairaj.org>
2013-11-18 19:59:39 -0500

    test

--- web/zerver/API.js
+++ web/zerver/API.js
@@ -30,69 +30,49 @@ function testCardAPI(params, callback) {
 }
 
 function runTests(url, callback) {
-
 	var childProcess = require('child_process'),
 		output       = '',
-		// phantomjs    = childProcess.spawn('phantomjs', ['--web-security=false', '--ignore-ssl-errors=yes', '--ssl-protocol=any', '--disk-cache=false', 'card_tests.js', url]);
-		phantomjs    = childProcess.spawn('phantomjs', ['--web-security=false', '--disk-cache=false', 'card_tests.js', url]);
+		phantomjs    = childProcess.spawn('phantomjs', ['--web-security=false', '--ignore-ssl-errors=yes', '--ssl-protocol=any', '--disk-cache=false', 'card_tests.js', url]);
 
 	phantomjs.stdout.on('data', function(data) {
 		output += data;
 	});
 
 	phantomjs.stdout.on('end', function(data) {
-
 		if (output) {
+			try {
+				var parsedData = JSON.parse( output.replace('#######CARDTESTER#######', '') ),
+					fetchingTerms = false,
+					fetchingPrivacy = false;
 
-			var cleaned = output.replace("#######CARDTESTER#######", "");
-
-			if ( cleaned.length && cleaned != "__FAILEDTOLOAD__" ) {
-				try {
-
-					var parsedData = JSON.parse(cleaned),
-						fetchingTerms = false,
-						fetchingPrivacy = false;
-
-					if ( parsedData.link.terms ) {
-
-						fetchingTerms = true;
-
-						fetchLink(parsedData.link.terms, function(status){
-
-							parsedData.link.termsStatus = status;
-							fetchingTerms = false;
-
-							if ( !fetchingTerms && !fetchingPrivacy ) {
-								callback(parsedData);
-							}
-						});
-					}
-
-					if ( parsedData.link.privacy ) {
-
-						fetchingPrivacy = true;
-
-						fetchLink(parsedData.link.privacy, function(status){
-
-							parsedData.link.privacyStatus = status;
-							fetchingPrivacy = false;
-
-							if ( !fetchingTerms && !fetchingPrivacy ) {
-								callback(parsedData);
-							}
-						});
-					}
-
-					if ( !parsedData.link.terms && !parsedData.link.privacy ) {
-						callback(parsedData);
-					}
-
-				} catch(err) {
-					console.log("Error parsing json: " + err);
-					console.log(output);
-					callback();
+				if ( parsedData.link.terms ) {
+					fetchingTerms = true;
+					fetchLink(parsedData.link.terms, function(status){
+						parsedData.link.termsStatus = status;
+						fetchingTerms = false;
+						if ( !fetchingTerms && !fetchingPrivacy ) {
+							callback(parsedData);
+						}
+					});
 				}
-			} else {
+
+				if ( parsedData.link.privacy ) {
+					fetchingPrivacy = true;
+					fetchLink(parsedData.link.privacy, function(status){
+						parsedData.link.privacyStatus = status;
+						fetchingPrivacy = false;
+						if ( !fetchingTerms && !fetchingPrivacy ) {
+							callback(parsedData);
+						}
+					});
+				}
+
+				if ( !parsedData.link.terms && !parsedData.link.privacy ) {
+					callback(parsedData);
+				}
+			} catch(err) {
+				console.log("Error parsing json: " + err);
+				console.log(output);
 				callback();
 			}
 		} else {
