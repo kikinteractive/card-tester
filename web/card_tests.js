657f668da9000fac1a21be0f8d5e0703b1f734c0
Jairaj Sethi <j@jairaj.org>
2013-11-24 22:44:28 -0500

    untest

--- web/card_tests.js
+++ web/card_tests.js
@@ -91,8 +91,6 @@ function preparePage(url, callback) {
 	};
 
 	page.onResourceRequested = function (req) {
-		console.log('PAGE   : ' + page.url);
-		console.log('REQUEST: ' + req.url);
 		page.resources[req.id] = {
 			request: req,
 			startReply: null,
