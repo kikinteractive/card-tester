a9c7f31520df477dd8f410264f5fd58a60de628d
Jairaj Sethi <j@jairaj.org>
2013-11-24 22:37:04 -0500

    requests for current page dont count for domload

--- web/card_tests.js
+++ web/card_tests.js
@@ -95,7 +95,7 @@ function preparePage(url, callback) {
 			request: req,
 			startReply: null,
 			endReply: null,
-			domLoaded: domLoaded
+			domLoaded: (req.url === page.url ? false : domLoaded)
 		};
 	};
 
