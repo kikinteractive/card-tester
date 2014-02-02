8006b200f6cfa4c5a5760355beefada4fa24488c
Jairaj Sethi <j@jairaj.org>
2014-02-02 00:15:59 -0500

    fix navigation bug for script detection

--- testPage.js
+++ testPage.js
@@ -53,6 +53,7 @@ function preparePage(url, callback) {
 	page.appCache = false;
 	page.webSecurityEnabled = false;
 	page.viewportSize = { width: 320, height : 548 };
+	page.navigationLocked = true;
 
 	page.onConsoleMessage = function (msg) {
 		if (msg.indexOf(logFilter) === 0) {
